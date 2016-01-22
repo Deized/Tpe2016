5e9e0af518884ac416d6bd82147f9234d12f6fab
Deized <xboxpuissance4@outlook.fr>
2016-01-22 17:16:54 +0000

    Tpe2016
    One

---
+++ histoire/histoire.php
@@ -0,0 +1,55 @@
+﻿<html>
+
+<head>
+<title> TPE-PI Histoire </title>
+<link rel="stylesheet" type="text/css" href="http://localhost:8888/css/style.css" media="all"/>
+<link rel="stylesheet" type="text/css" href="http://localhost:8888/SpecialFonts/css/font-awesome.min.css" media="all"/>
+<link rel="stylesheet" type="text/css" href="http://localhost:8888/SpecialFonts/css/font-awesome.css" media="all"/>
+</head>
+
+<body id="bod">
+<div id="aide" style="width:160px" onmouseover="help();" onmouseout="helpOut();">
+<div id="robot"><img src="http://localhost:8888/robots/Alpha1.png" style="position:absolute;"></div>
+<i class="fa fa-info-circle" style="margin-left: 120px;margin-top: 15px;"></i>
+</div>
+<div id="aide_text">Pour naviguer dans cette frise chronologique de l'informatque il suffit d'utiliser les différents boutons en bas, chacun étant dédié à une période. Sinon vous pouvez aussi cliquez n'importe où sur l'écran pour suivre pas à pas l'Histoire de l'informatique. Quand vous aurez finis, le bouton en bas à droite vous permettra de passer à la suite.</div>
+<div id="zbox" style="height: 80%;">
+<div id="espace1" style="
+    height: 22%;
+"></div>
+<div id="box_preamb"></div>
+</div>
+
+<div id="frise">
+<div id="plus_info" style="
+    padding-left: 4%;
+    padding-right: 3%;
+    height: 80%;
+">
+<div id="plus_info2">
+<div id="head1"></div>
+<div id="bodi"></div>
+</div>
+</div>
+<div id="rect">
+<div id="point1_h" class="point_h" onmouseover="appear(0);" onmouseout="disapear();" onclick="NextChapt('1');"></div>
+<div id="point2_h" class="point_h" onmouseover="appear(1);" onmouseout="disapear();" onclick="NextChapt('2');"></div>
+<div id="point3_h" class="point_h" onmouseover="appear(2);" onmouseout="disapear();" onclick="NextChapt('3');"></div>
+<div id="point4_h" class="point_h" onmouseover="appear(3);" onmouseout="disapear();" onclick="NextChapt('4');"></div>
+<div id="point5_h" class="point_h" onmouseover="appear(4);" onmouseout="disapear();" onclick="NextChapt('5');"></div>
+<div id="point6_h" class="point_h" onmouseover="appear(5);" onmouseout="disapear();" onclick="NextChapt('6');"></div>
+<div id="point7_h" class="point_h" onmouseover="appear(6);" onmouseout="disapear();" onclick="NextChapt('7');"></div>
+<div id="point8_h" class="point_h" onmouseover="appear(7);" onmouseout="disapear();" onclick="NextChapt('8');"></div>
+<div id="point9_h" class="point_h" onmouseover="appear(8);" onmouseout="disapear();" onclick="NextChapt('9');"></div>
+<div id="next"><div style="height:30%;"></div><i class="fa fa-arrow-right"></i></div>
+</div>
+</div>
+
+</body>
+
+<footer>
+<script type="text/javascript" src="http://localhost:8888/scripts/functions.js"></script>
+<script type="text/javascript" src="http://localhost:8888/scripts/scriptH.js"></script>
+</footer>
+
+</html>
---
+++ histoire/preamb.php
@@ -0,0 +1,10 @@
+La programmation informatique, un concept assez 
+flou pour certains. Pourtant, elle est utilisée partout dans notre quotidien. 
+L'alarme qui vous réveille le matin au train qui vous conduit à votre lieu du travail en passant 
+par les dizaines d'appels téléphoniques que vous passez chaque jour.
+En somme, l'écran que vous avez devant les yeux n'est pas si compliqué que ça. Ce ne sont que des pixels, des bouts de 
+lumières collés les uns aux autres pour former une image.
+Ce qui devient intéressant c'est la technique employée afin que l'écran traite avec l'être humain.
+La programmation informatique rentre en jeu. Sans vous en dire plus pour l'instant, je vous laisse avec <span style="color:brown;">Alpha#2</span> qui vous présentera une frise chronologique
+expliquant en détails l'évolution passée de la programmation informatique.
+"S'appuyer sur l'expérience du passé devrait suffire à démontrer que la plupart des révolutions technologiques sont issues de recherches dont la seule motivation était le progrés de la connaissance." a dit Pierre Joliot, scientifique du XXème siècle.
